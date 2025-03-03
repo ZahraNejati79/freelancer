f945315357d735b5811e45730c6cea593de8b6a4
zahraNejati <nejatizahra4@gmail.com>
2025-03-03 21:27:12 +0330

    custom input component - TextField

--- src/features/authentication/SendOTPForm.tsx
+++ src/features/authentication/SendOTPForm.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+import TextField from "../../ui/TextField";
+
 const SendOTPFrom: React.FC = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
   return (
     <form className="space-y-8">
-      <div>
-        <label>شماره همراه</label>
-        <input className="textField__input" />
-      </div>
+      <TextField
+        name="phoneNumber"
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+        label="شماره همراه"
+      />
       <button type="submit" className="btn btn--primary">
         ارسال کد تایید
       </button>
