9f64b8a22587df6ad59c80a49508eb14aac23d8d
zahra nejati <neejati@gmail.com>
2025-03-04 10:32:12 +0330

    [bugfix] field name in auth service

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,9 +1,9 @@
 import http from "./httpService";
 
-export function getOtp(data: { phomeNumber: string }) {
-  return http.post("/user/get-otp", data);
+export function getOtp(data: { phoneNumber: string }) {
+  return http.post("/user/get-otp", data).then(({ data }) => data.data);
 }
 
-export function checkOtp(data: { phomeNumber: string; otp: string }) {
+export function checkOtp(data: { phoneNumber: string; otp: string }) {
   return http.post("/user/check-otp", data);
 }
